82d177163f674a7ede73c727e900ff660b549066
Dario Alvarez <daralv53@gmail.com>
2024-07-30 16:41:43 -0600

    creando funcion para agregar items a la orden

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -2,14 +2,14 @@ import { MenuItems } from "../types"
 
 interface MenuItemsProps {
     item: MenuItems;
-    addItem: () => void //esto es una funcion que no retorna nada
+    addItem: (item: MenuItems) => void //esto es una funcion que no retorna nada
 }
 
 export default function MenuItem({item, addItem} : MenuItemsProps) {
     return (
         <button
             className="border-2 border-blue-500 hover:bg-blue-500 hover:text-white w-full p-3 flex justify-between rounded-md"
-            onClick={() => addItem()}
+            onClick={() => addItem(item)}
         >
             <p>{item.name}</p>
             <p className="font-black">${item.price}</p>
--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,13 +1,32 @@
 import { useState } from "react"
-import type { OrderItem } from "../types"
+import type { MenuItems, OrderItem } from "../types"
 
 export default function useOrder() {
 
-    const[order, setOrder] = useState<OrderItem>([]);
+    const[order, setOrder] = useState<OrderItem[]>([]);
 
-    const addItem = () => {
-        console.log('Agregando...')
+    //Fncion para agregar items al cosnumo
+    const addItem = (item: MenuItems) => {
+        //Checa si un item ya existe y solo cambiar la cantidad, y si no existe agregarlo
+        const itemExist = order.find(orderItem => orderItem.id === item.id)
+        //Si el item existe realiza la tarea de aumentar la cantidad
+        if(itemExist) {
+            const updatedOrder = order.map(orderItem => orderItem.id === item.id ? 
+                {...orderItem, quantity: orderItem.quantity + 1} : 
+                orderItem
+        )
+        setOrder(updatedOrder)
+
+        //Si no existe agrega el nuevo producto
+        } else {
+            const newItem : OrderItem = {...item, quantity: 1}        
+            setOrder([...order, newItem])
+        }
+
+        
     }
+
+    console.log(order)
 
     return {
         addItem
